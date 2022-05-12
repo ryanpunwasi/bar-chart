b9190e04e53327936a5e8c7f15dfe757a2f2c551
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-05-11 21:30:06 -0400

    Boilerplate code for chart rendering function

---
+++ drawBarChart.js
@@ -0,0 +1,50 @@
+const drawBarChart = (data, options, element) => {
+
+  const renderBars = data => {
+
+  }
+
+  const renderTitle = (title, color = 'black', size = '2em') => {
+    return `<p style="color: ${color}; font-size: ${size}">${toTitleCase(title)}</p>`;
+  };
+
+  const renderXAxis = () => {
+
+  }
+
+  const renderYAxis = () => {
+
+  }
+
+  const renderXAxisTitle = title => {
+
+  }
+
+  const renderYAxisTitle = title => {
+
+  }
+
+  // Render calls
+
+  $(element).append(renderTitle(options['title']));
+
+}
+
+// Helper functions
+
+const toTitleCase = text => {
+  const words = text.trim().split(" ");
+  let title = '';
+  for(let i = 0; i < words.length; i++) {
+    title += words[i][0].toUpperCase() + words[i].slice(1) + " ";
+  }
+  return title.trimEnd();
+}
+
+const data = [1, 2, 3];
+const options = { title: "Chart Title" };
+const element = document.getElementById('chart');
+
+$( document ).ready(function() {
+  drawBarChart(data, options, element);
+});
